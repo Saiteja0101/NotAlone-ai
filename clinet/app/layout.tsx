8f1b32149015a89b74f262c00400e32e44e23ec8
Chippa Saiteja <csaiteja64@gmail.com>
2025-08-21 16:20:39 +0530

    added logo

--- clinet/app/layout.tsx
+++ clinet/app/layout.tsx
@@ -4,6 +4,9 @@ import "../styles/globals.css";
 export const metadata: Metadata = {
   title: "NotAlone AI",
   description: "Share your feelings with AI",
+  icons: {
+    icon: '/NotAlone-ai.png'
+  }
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
