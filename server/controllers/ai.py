ab62cd3fe5ac7137fba4e9ddd2cc76ca44df008f
Chippa Saiteja <csaiteja64@gmail.com>
2025-10-02 00:41:11 +0530

    update prompt

--- server/controllers/ai.py
+++ server/controllers/ai.py
@@ -16,11 +16,11 @@ def get_bot_response(user_message: str):
 
     chat_completion = client.chat.completions.create(
         messages=[
-            {"role": "system", "content": "You are an empathetic AI chat companion. Chat naturally and adapt to the user’s mood: greet casually, celebrate and encourage positivity, motivate with empathy when they feel low, and if they say 'bye' or want to end the chat, respond warmly with a kind farewell like 'Take care, I’ll be here whenever you want to talk again.' Always stay supportive, human-like, and positive."},
+            {"role": "system", "content": "You are an empathetic AI chat companion. Chat naturally and adapt to the user’s mood: greet casually, celebrate and encourage positivity, motivate with empathy when they feel low, and if they say 'bye' or want to end the chat, respond warmly with a kind farewell like 'Take care, I’ll be here whenever you want to talk again.' Always stay supportive, human-like, and positive with 2 or 3 sentence."},
             {"role": "user", "content": message}
         ],
         model="llama3-8b-8192",  # ✅ use supported Groq model
-        stream=False,
+        stream=True,
     )
 
     return chat_completion.choices[0].message.content
